2ba33a282e1f0531111a0e34aaa4d0b90ae64d24
Guenter Hipler <guenter.hipler@unibas.ch>
2019-02-27 22:07:47 +0100

    prepare tomcat in develop mode

--- scripts/movejar.sh
+++ scripts/movejar.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 mv ./build/libs/SRWSearchDocumentsCatcher.jar ./build/libs/SRWSearchDocumentsCatcher.aar
+
+cp -p ./build/libs/SRWSearchDocumentsCatcher.aar /home/swissbib/env.local/tools/java.tools/tomcat7-axis2/webapps/axis2/WEB-INF/services
